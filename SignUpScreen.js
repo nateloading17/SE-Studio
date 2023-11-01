0ea8671ba54aa9cc06887ac1ff88aa44d7a2730a
nateloading17 <72894219+nateloading17@users.noreply.github.com>
2023-11-01 14:38:01 -0400

    Update SignUpScreen.js

--- SignUpScreen.js
+++ SignUpScreen.js
@@ -1,84 +1,152 @@
 import React, { useState } from 'react';
 
-function SignUpScreen({ onSignUp, onCancel }) {
+function SignUpScreen() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [birthdayMonth, setBirthdayMonth] = useState('');
+  const [birthdayDay, setBirthdayDay] = useState('');
+  const [birthdayYear, setBirthdayYear] = useState('');
+  const [gender, setGender] = useState('');
   const [securityQuestion1, setSecurityQuestion1] = useState('');
   const [securityAnswer1, setSecurityAnswer1] = useState('');
   const [securityQuestion2, setSecurityQuestion2] = useState('');
   const [securityAnswer2, setSecurityAnswer2] = useState('');
 
-  const handleSignUp = () => {
-    // Add your logic for signing up here
-    if (
-      username &&
-      password &&
-      confirmPassword &&
-      securityQuestion1 &&
-      securityAnswer1 &&
-      securityQuestion2 &&
-      securityAnswer2
-    ) {
-      if (password === confirmPassword) {
-        onSignUp(username);
-      } else {
-        alert('Passwords do not match.');
-      }
-    } else {
-      alert('Please fill in all fields.');
-    }
-  };
-
   return (
     <div>
-      <h1>Sign Up</h1>
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Confirm Password"
-        value={confirmPassword}
-        onChange={(e) => setConfirmPassword(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Security Question 1"
-        value={securityQuestion1}
-        onChange={(e) => setSecurityQuestion1(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Answer to Question 1"
-        value={securityAnswer1}
-        onChange={(e) => setSecurityAnswer1(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Security Question 2"
-        value={securityQuestion2}
-        onChange={(e) => setSecurityQuestion2(e.target.value)}
-      />
-      <input
-        type="text"
-        placeholder="Answer to Question 2"
-        value={securityAnswer2}
-        onChange={(e) => setSecurityAnswer2(e.target.value)}
-      />
-      <button onClick={handleSignUp}>Sign Up</button>
-      <button onClick={onCancel}>Cancel</button>
+      <h2>Sign Up</h2>
+      <div>
+        <label>
+          First Name:
+          <input
+            type="text"
+            placeholder="Enter your first name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </label>
+        <label>
+          Last Name:
+          <input
+            type="text"
+            placeholder="Enter your last name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </label>
+        <label>
+          Email Address:
+          <input
+            type="email"
+            placeholder="Enter your email address"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Username:
+          <input
+            type="text"
+            placeholder="Enter your username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <label>
+          Confirm Password:
+          <input
+            type="password"
+            placeholder="Confirm your password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Birthday:
+          <select value={birthdayMonth} onChange={(e) => setBirthdayMonth(e.target.value)}>
+            {/* Dropdown options for months */}
+          </select>
+          <select value={birthdayDay} onChange={(e) => setBirthdayDay(e.target.value)}>
+            {/* Dropdown options for days */}
+          </select>
+          <select value={birthdayYear} onChange={(e) => setBirthdayYear(e.target.value)}>
+            {/* Dropdown options for years */}
+          </select>
+        </label>
+        <label>
+          Gender:
+          <select value={gender} onChange={(e) => setGender(e.target.value)}>
+            <option value="female">Female</option>
+            <option value="male">Male</option>
+            <option value="preferNotToSay">Prefer Not to Say</option>
+          </select>
+        </label>
+      </div>
+      <div>
+        <label>
+          Security Question 1:
+          <select value={securityQuestion1} onChange={(e) => setSecurityQuestion1(e.target.value)}>
+            {/* Dropdown options for security questions */}
+          </select>
+        </label>
+        <label>
+          Security Question 1 Answer:
+          <input
+            type="text"
+            placeholder="Enter your answer"
+            value={securityAnswer1}
+            onChange={(e) => setSecurityAnswer1(e.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Security Question 2:
+          <select value={securityQuestion2} onChange={(e) => setSecurityQuestion2(e.target.value)}>
+            {/* Dropdown options for security questions */}
+          </select>
+        </label>
+        <label>
+          Security Question 2 Answer:
+          <input
+            type="text"
+            placeholder="Enter your answer"
+            value={securityAnswer2}
+            onChange={(e) => setSecurityAnswer2(e.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <button onClick={handleSignUp}>Submit</button>
+      </div>
     </div>
   );
 }
 
+    return (
+        <div>
+          <h1>My Application</h1>
+          {showConfirmation ? (
+            <ConfirmationScreen /> // Conditionally render the confirmation screen
+          ) : (
+            <SignUpScreen handleSignUp={handleSignUp} /> // Pass the handleSignUp function as a prop
+          )}
+        </div>
+      );
+    
 export default SignUpScreen;
