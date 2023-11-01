c5e49c1c4e1dc113a322b3f16aa6780e1cdac047
nateloading17 <72894219+nateloading17@users.noreply.github.com>
2023-10-31 20:00:21 -0400

    Create ConfirmationScreen.js
    Account creation confirmation screen mockup

---
+++ ConfirmationScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ConfirmationScreen({ onCancel }) {
+  return (
+    <div>
+      <h1>Account Created Successfully</h1>
+      <p>Your account has been created successfully.</p>
+      <button onClick={onCancel}>Back to Home</button>
+    </div>
+  );
+}
+
+export default ConfirmationScreen;
